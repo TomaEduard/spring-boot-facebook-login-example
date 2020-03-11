b7fda0077abc3d6ff1be3862e01326be3a89f87e
Eduard Toma <eduard.daniel.toma@gmail.com>
2020-03-11 10:55:47 +0200

    initial commit

---
+++ src/test/java/com/company/security/springbootfacebookloginexample/SpringBootFacebookLoginExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.security.springbootfacebookloginexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFacebookLoginExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
